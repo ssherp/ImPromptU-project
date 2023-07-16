94444b5f9cca8958075eaf4d0b645a4f6d7a1191
ssherp <ssherpa89@gmail.com>
2023-07-15 22:41:59 -0700

    debugged so u dont have trail of iss

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,24 @@ var map = L.map('map').setView([0, 0], 3);
 
 var lmark;
 var myIcon = L.icon({
-        iconUrl: './assets/image/astronaut.png',
-        iconSize: [60, 60]
-        
-    });
-    
+    iconUrl: './assets/image/astronaut.png',
+    iconSize: [60, 60]
+
+});
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 
-    
-    
-
 }).addTo(map);
-//var lmark = L.marker([10, 10],{icon:myIcon}).addTo(map)
 
-var updateMarker = function(newLat, newLng) {
-    var lmark = L.marker([10, 10],{icon:myIcon}).addTo(map)
-    lmark.setLatLng([newLat, newLng]);
-    map.panTo([newLat, newLng], animate = true);
-}
+var updateMarker = function (newLat, newLng) {
+    if (lmark) {
+      lmark.setLatLng([newLat, newLng]);
+    } else {
+      lmark = L.marker([newLat, newLng], { icon: myIcon }).addTo(map);
+    }
+    map.panTo([newLat, newLng], { animate: true });
+  };
 
 var requestIssUrl = 'http://api.open-notify.org/iss-now.json';
 function issFetch() {
@@ -34,15 +33,15 @@ function issFetch() {
             var latCord = data.iss_position.latitude;
             var lonCord = data.iss_position.longitude;
             satCords.textContent = "Latitude: " + latCord + " / Longitude: " + lonCord;
-            var lmark = L.marker([latCord, lonCord],{icon:myIcon}).addTo(map)
-            updateMarker(latCord,lonCord)
+            updateMarker(latCord, lonCord)
         })
 }
 
 issFetch();
 
 
-setInterval(function () { $(".time").text(dayjs().format("MMM DD YYYY  H:mm:ss")) },
+setInterval(function () {
+     $(".time").text(dayjs().format("MMM DD YYYY  H:mm:ss")) },
     1000);
 setInterval(issFetch, 5000)
 
@@ -57,19 +56,19 @@ fetch(geoRequest)
         console.log(data)
         cityLat = data.results[0].locations[0].displayLatLng.lat;
         cityLon = data.results[0].locations[0].displayLatLng.lng;
-
+        var issDistance = haversine(cityLat, cityLon, 0, 0);
         // cityLocation(cityLat,cityLon)
         console.log(cityLat, cityLon)
     })
-var sunRequest="https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today"
+var sunRequest = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today"
 fetch(sunRequest)
-.then(function(response){
-    return response.json()
-})
-.then(function(data){
-    console.log(data)
-//cityRise
-})
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        //cityRise
+    })
 
 var haversine = function (lat1, lon1, lat2, lon2) {
     var radius = 6371; //kilometers
